5a7ee132b0535cd14950e339517054c8dccc029e
miyazakimaiko <myzkmik19922@gmail.com>
2021-06-15 15:57:12 +0100

    Added nav animation

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,58 @@
-// custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
+const React = require("react")
+
+
+// Logs when the client route changes
+exports.onRouteUpdate = () => {
+
+    let menuIcon = document.getElementById("humberger-menu-icon")
+
+    menuIcon.onclick = () => {
+
+        menuIcon.classList.toggle('hover')
+
+        let navScreen = document.getElementById("nav-screen")
+    
+        if (navScreen.style.display === 'none') {
+      
+          // Show background
+          navScreen.style.display = "flex"
+    
+          let mainArea = document.querySelector("main")
+          mainArea.style.display = "none"
+      
+          // fix the header
+          let header = document.querySelector("header")
+          header.style.position = "fixed"
+      
+          let logo = document.getElementById("header-logo")
+          logo.style.display = "none"
+      
+          let footerArea = document.querySelector("footer")
+      
+          if (footerArea) {
+            footerArea.style.display = "none"
+          }
+      
+        } else {
+      
+          // Then hide background
+          navScreen.style.display = "none";
+      
+          let mainArea = document.querySelector("main")
+          mainArea.style.display = "flex"
+      
+          // unfix the header
+          let header = document.querySelector("header");
+          header.style.position = "absolute";
+      
+          let logo = document.getElementById("header-logo")
+          logo.style.display = "block"
+      
+          let footerArea = document.querySelector("footer")
+      
+          if (footerArea) {
+            footerArea.style.display = "block"
+          }
+        }
+      }
+  }
