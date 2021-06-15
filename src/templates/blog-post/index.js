c4f85cde76c0ad8945ddc2b71ef0ff28020755c3
miyazakimaiko <myzkmik19922@gmail.com>
2021-06-15 16:01:14 +0100

    Broke lines for improving readability

--- src/templates/blog-post/index.js
+++ src/templates/blog-post/index.js
@@ -4,9 +4,11 @@ import Layout from '../../components/layout';
 import SEO from '../../components/seo';
 import Readnext from './readnext'
 import './blog-post.css';
+import 'gatsby-remark-vscode/styles.css';
 
 const BlogTemplate = (props) => {
     const post = props.data.markdownRemark
+    const siteMeta = props.data.site.siteMetadata
     return (
         <Layout>
             <SEO 
@@ -36,29 +38,162 @@ const BlogTemplate = (props) => {
                 </section>
 
                 <article>
-                    <div className="article-contents" dangerouslySetInnerHTML={
+                    <div className="article-contents post-contents" dangerouslySetInnerHTML={
                         {__html: `${post.html}`}
                     } />
                 </article>
 
                 <section class="sharing-buttons-section">
                     <ul class="sharing-button-list">
+                        {/* ================ Twitter button ================ */}
                         <li class="sharing-button">
-                            <a href="">
-                                <svg data-icon="twitter" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="sns-icon"><path fill="currentColor" d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z" class=""></path></svg>
-                                <span>Twitter</span></a></li>
+                            <a 
+                            href={`https://twitter.com/intent/tweet?text=${post.frontmatter.title}&url=${siteMeta.siteUrl}/blog/${post.frontmatter.slug}`}
+                            >
+                                <svg 
+                                    data-icon="twitter" 
+                                    role="img" 
+                                    xmlns="http://www.w3.org/2000/svg" 
+                                    viewBox="0 0 512 512" 
+                                    class="sns-icon"
+                                >
+                                    <path 
+                                        fill="currentColor" 
+                                        d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 
+                                        0 138.72-105.583 298.558-298.558 298.558-59.452 
+                                        0-114.68-17.219-161.137-47.106 8.447.974 16.568 
+                                        1.299 25.34 1.299 49.055 0 94.213-16.568 
+                                        130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 
+                                        6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 
+                                        27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 
+                                        7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 
+                                        0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 
+                                        105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 
+                                        0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 
+                                        76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 
+                                        24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 
+                                        60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z" class="">
+                                    </path>
+                                </svg>
+                                <span>Twitter</span>
+                            </a>
+                        </li>
+
+                        {/* ================ Facebook button ================ */}
                         <li class="sharing-button">
-                            <a href=""><svg data-icon="facebook" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="sns-icon"><path fill="currentColor" d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z" class=""></path></svg>
-                                <span>Facebook</span></a></li>
-                        <li class="sharing-button"><a href="">
-                            <svg data-icon="linkedin-in" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="sns-icon"><path fill="currentColor" d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z" class=""></path></svg>
-                            <span>Linkedin</span></a></li>
-                        <li class="sharing-button"><a href=""><svg data-icon="reddit" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="sns-icon"><path fill="currentColor" d="M201.5 305.5c-13.8 0-24.9-11.1-24.9-24.6 0-13.8 11.1-24.9 24.9-24.9 13.6 0 24.6 11.1 24.6 24.9 0 13.6-11.1 24.6-24.6 24.6zM504 256c0 137-111 248-248 248S8 393 8 256 119 8 256 8s248 111 248 248zm-132.3-41.2c-9.4 0-17.7 3.9-23.8 10-22.4-15.5-52.6-25.5-86.1-26.6l17.4-78.3 55.4 12.5c0 13.6 11.1 24.6 24.6 24.6 13.8 0 24.9-11.3 24.9-24.9s-11.1-24.9-24.9-24.9c-9.7 0-18 5.8-22.1 13.8l-61.2-13.6c-3-.8-6.1 1.4-6.9 4.4l-19.1 86.4c-33.2 1.4-63.1 11.3-85.5 26.8-6.1-6.4-14.7-10.2-24.1-10.2-34.9 0-46.3 46.9-14.4 62.8-1.1 5-1.7 10.2-1.7 15.5 0 52.6 59.2 95.2 132 95.2 73.1 0 132.3-42.6 132.3-95.2 0-5.3-.6-10.8-1.9-15.8 31.3-16 19.8-62.5-14.9-62.5zM302.8 331c-18.2 18.2-76.1 17.9-93.6 0-2.2-2.2-6.1-2.2-8.3 0-2.5 2.5-2.5 6.4 0 8.6 22.8 22.8 87.3 22.8 110.2 0 2.5-2.2 2.5-6.1 0-8.6-2.2-2.2-6.1-2.2-8.3 0zm7.7-75c-13.6 0-24.6 11.1-24.6 24.9 0 13.6 11.1 24.6 24.6 24.6 13.8 0 24.9-11.1 24.9-24.6 0-13.8-11-24.9-24.9-24.9z" class=""></path></svg>
-                            <span>Reddit</span></a></li>
-                        <li class="sharing-button"><a href=""><svg data-icon="hacker-news" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="sns-icon"><path fill="currentColor" d="M0 32v448h448V32H0zm21.2 197.2H21c.1-.1.2-.3.3-.4 0 .1 0 .3-.1.4zm218 53.9V384h-31.4V281.3L128 128h37.3c52.5 98.3 49.2 101.2 59.3 125.6 12.3-27 5.8-24.4 60.6-125.6H320l-80.8 155.1z" class=""></path></svg>
-                            <span>Hacker News</span></a></li>
-                        <li class="sharing-button"><a href=""><svg data-icon="envelope" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="sns-icon"><path fill="currentColor" d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z" class=""></path></svg>
-                            <span>Email</span></a></li>
+                            <a href={`https://www.facebook.com/sharer/sharer.php?text=${post.frontmatter.title}&u=${siteMeta.siteUrl}/blog/${post.frontmatter.slug}`}>
+                                <svg 
+                                    data-icon="facebook" 
+                                    role="img" 
+                                    xmlns="http://www.w3.org/2000/svg" 
+                                    viewBox="0 0 512 512" class="sns-icon"
+                                >
+                                    <path 
+                                        fill="currentColor" 
+                                        d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 
+                                        245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 
+                                        4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 
+                                        71.69h-57.78V501C413.31 482.38 504 379.78 504 256z" class="">
+                                    </path>
+                                </svg>
+                                <span>Facebook</span>
+                            </a>
+                        </li>
+
+                        {/* ================ Linkedin button ================ */}
+                        <li class="sharing-button">
+                            <a href={`https://www.linkedin.com/shareArticle?mini=true&url=${siteMeta.siteUrl}/blog/${post.frontmatter.slug}&smid=li-share&title=${post.frontmatter.title}`}>
+                                <svg 
+                                    data-icon="linkedin-in" 
+                                    role="img" 
+                                    xmlns="http://www.w3.org/2000/svg" 
+                                    viewBox="0 0 448 512" 
+                                    class="sns-icon"
+                                >
+                                    <path 
+                                        fill="currentColor" 
+                                        d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 
+                                        53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 
+                                        448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 
+                                        76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 
+                                        111.28 61.9 111.28 142.3V448z" class="">
+                                    </path>
+                                </svg>
+                                <span>Linkedin</span>
+                            </a>
+                        </li>
+
+                        {/* ================ Reddit button ================ */}
+                        <li class="sharing-button">
+                            <a href={`https://reddit.com/submit?url=${siteMeta.siteUrl}/blog/${post.frontmatter.slug}/&smid=re-share&%20%20%20%20%20%20%20%20title=${post.frontmatter.title}`}>
+                                <svg 
+                                    data-icon="reddit" 
+                                    role="img" 
+                                    xmlns="http://www.w3.org/2000/svg" 
+                                    viewBox="0 0 512 512" class="sns-icon"
+                                >
+                                    <path 
+                                        fill="currentColor" 
+                                        d="M201.5 305.5c-13.8 0-24.9-11.1-24.9-24.6 0-13.8 11.1-24.9 24.9-24.9 13.6 0 24.6 
+                                        11.1 24.6 24.9 0 13.6-11.1 24.6-24.6 24.6zM504 256c0 137-111 248-248 248S8 393 8 256 
+                                        119 8 256 8s248 111 248 248zm-132.3-41.2c-9.4 0-17.7 3.9-23.8 10-22.4-15.5-52.6-25.5-86.1-26.6l17.4-78.3 
+                                        55.4 12.5c0 13.6 11.1 24.6 24.6 24.6 13.8 0 24.9-11.3 24.9-24.9s-11.1-24.9-24.9-24.9c-9.7 
+                                        0-18 5.8-22.1 13.8l-61.2-13.6c-3-.8-6.1 1.4-6.9 4.4l-19.1 86.4c-33.2 1.4-63.1 11.3-85.5 
+                                        26.8-6.1-6.4-14.7-10.2-24.1-10.2-34.9 0-46.3 46.9-14.4 62.8-1.1 5-1.7 10.2-1.7 15.5 0 52.6 
+                                        59.2 95.2 132 95.2 73.1 0 132.3-42.6 132.3-95.2 0-5.3-.6-10.8-1.9-15.8 31.3-16 19.8-62.5-14.9-62.5zM302.8 
+                                        331c-18.2 18.2-76.1 17.9-93.6 0-2.2-2.2-6.1-2.2-8.3 0-2.5 2.5-2.5 6.4 0 8.6 22.8 22.8 87.3 22.8 
+                                        110.2 0 2.5-2.2 2.5-6.1 0-8.6-2.2-2.2-6.1-2.2-8.3 0zm7.7-75c-13.6 0-24.6 11.1-24.6 24.9 0 13.6 
+                                        11.1 24.6 24.6 24.6 13.8 0 24.9-11.1 24.9-24.6 0-13.8-11-24.9-24.9-24.9z" class="">
+                                    </path>
+                                </svg>
+                                <span>Reddit</span>
+                            </a>
+                        </li>
+
+                        {/* ================ Hacker News button ================ */}
+                        <li class="sharing-button">
+                            <a href={`https://news.ycombinator.com/submitlink?u=${siteMeta.siteUrl}/blog/${post.frontmatter.slug}/`}>
+                                <svg 
+                                    data-icon="hacker-news" 
+                                    role="img" 
+                                    xmlns="http://www.w3.org/2000/svg" 
+                                    viewBox="0 0 448 512" 
+                                    class="sns-icon"
+                                >
+                                    <path 
+                                        fill="currentColor" 
+                                        d="M0 32v448h448V32H0zm21.2 197.2H21c.1-.1.2-.3.3-.4 0 .1 0 .3-.1.4zm218 
+                                        53.9V384h-31.4V281.3L128 128h37.3c52.5 98.3 49.2 101.2 59.3 125.6 12.3-27 
+                                        5.8-24.4 60.6-125.6H320l-80.8 155.1z" class="">
+                                    </path>
+                                </svg>
+                                <span>Hacker News</span>
+                            </a>
+                        </li>
+
+                        {/* ================ Email button ================ */}
+                        <li class="sharing-button">
+                            <a href={`mailto:?subject=${post.frontmatter.title} | Miyazakimaiko.com&body=${post.frontmatter.description}%0D%0A${siteMeta.siteUrl}/blog/${post.frontmatter.slug}/`}>
+                                <svg 
+                                    data-icon="envelope" 
+                                    role="img" 
+                                    xmlns="http://www.w3.org/2000/svg" 
+                                    viewBox="0 0 512 512" 
+                                    class="sns-icon"
+                                >
+                                    <path 
+                                        fill="currentColor" 
+                                        d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 
+                                        0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 
+                                        21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 
+                                        154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 
+                                        5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 
+                                        7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z" class="">
+                                    </path>
+                                </svg>
+                                <span>Email</span>
+                            </a>
+                        </li>
                     </ul>
                 </section>
 
@@ -76,6 +211,7 @@ export const query = graphql`
             siteMetadata {
               title
               author
+              siteUrl
             }
           }
         markdownRemark(frontmatter: { slug: { eq: $slug } }) {
@@ -83,6 +219,7 @@ export const query = graphql`
             html
             frontmatter {
                 title
+                description
                 date(formatString: "DD MMMM 'YYYY")
                 updatedAt(formatString: "DD MMMM 'YYYY")
                 featuredImage
