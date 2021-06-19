756aaa7dfca4124b4465da636854077950f95401
miyazakimaiko <myzkmik19922@gmail.com>
2021-06-19 19:00:04 +0100

    Added contents

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,7 +1,8 @@
 import * as React from "react"
 import './about.css'
 
-import ImgOldStores from '../../images/old-stores-second-floor.jpg';
+import store from '../../images/old-stores-second-floor.jpg';
+import face from '../../images/face.jpg';
 
 const About = () => (
     <div class="inner-screen-container">
@@ -9,91 +10,24 @@ const About = () => (
             backgroundImage: `linear-gradient(
                 to bottom,
                 rgba(0, 0, 0, 0.3), 
-                rgba(0, 0, 0, 0.3)), url(${ImgOldStores})`}}
+                rgba(0, 0, 0, 0.3)), url(${store})`}}
         >
             <h1 className="page-title">
-                A little more about me
+                About
             </h1>
         </section>
         <article>
-            <div class="article-contents">
-                <p>
-                    Thanks for visiting. My name is Maiko Miyazaki.
-                    This static website is developed for a imaginary company called "Travel" which sells travel equipment. 
-                    Created with HTML5, CSS3, Sass, plain JavaScript and jQuery.
-                </p>
-                <p>
-                    Aliquam erat volutpat. Orci varius natoque penatibus et magnis dis parturient montes, 
-                    nascetur ridiculus mus. In justo sapien, vestibulum in efficitur sit amet, fringilla posuere elit. 
-                    Vestibulum nec commodo nunc, in sagittis dui. Fusce bibendum mauris ac lacus vulputate, 
-                    et congue ligula volutpat. Donec id sem massa. Mauris ex lectus, posuere nec tempus ac, 
-                    iaculis rhoncus urna. Suspendisse sed cursus odio.
-                </p>
-                <p>
-                    Suspendisse condimentum bibendum erat, vel sodales mauris vulputate at. Fusce libero tellus, 
-                    porttitor sit amet tempor et, cursus in nibh. Donec ipsum arcu, convallis eu ex non, suscipit sollicitudin enim. 
-                    Ut a auctor ex. Phasellus volutpat ultrices neque eget cursus. In a erat venenatis turpis interdum facilisis. 
-                    Praesent mollis urna quis commodo accumsan. Phasellus interdum, ligula sit amet mattis rhoncus, 
-                    tellus ex semper mi, at congue orci tellus at nulla. Donec ac augue eu nibh feugiat iaculis. 
-                    Etiam laoreet dictum purus, at ultrices urna euismod vitae. Sed finibus orci a dictum dapibus. 
-                    Vivamus ullamcorper, sem quis porttitor dapibus, magna felis fringilla augue, sed elementum diam est nec justo. 
-                    Sed feugiat pellentesque enim, vitae viverra erat vestibulum eget. Morbi consequat accumsan tempus. 
-                    Etiam metus tellus, blandit a dignissim eget, vehicula non quam.
-                </p>
-                <p>
-                    Aliquam erat volutpat. Orci varius natoque penatibus et magnis dis parturient montes, 
-                    nascetur ridiculus mus. In justo sapien, vestibulum in efficitur sit amet, fringilla posuere elit. 
-                    Vestibulum nec commodo nunc, in sagittis dui. Fusce bibendum mauris ac lacus vulputate, 
-                    et congue ligula volutpat. Donec id sem massa. Mauris ex lectus, posuere nec tempus ac, 
-                    iaculis rhoncus urna. Suspendisse sed cursus odio.
-                </p>
-                <p>
-                    Suspendisse condimentum bibendum erat, vel sodales mauris vulputate at. Fusce libero tellus, 
-                    porttitor sit amet tempor et, cursus in nibh. Donec ipsum arcu, convallis eu ex non, suscipit sollicitudin enim. 
-                    Ut a auctor ex. Phasellus volutpat ultrices neque eget cursus. In a erat venenatis turpis interdum facilisis. 
-                    Praesent mollis urna quis commodo accumsan. Phasellus interdum, ligula sit amet mattis rhoncus, 
-                    tellus ex semper mi, at congue orci tellus at nulla. Donec ac augue eu nibh feugiat iaculis. 
-                    Etiam laoreet dictum purus, at ultrices urna euismod vitae. Sed finibus orci a dictum dapibus. 
-                    Vivamus ullamcorper, sem quis porttitor dapibus, magna felis fringilla augue, sed elementum diam est nec justo. 
-                    Sed feugiat pellentesque enim, vitae viverra erat vestibulum eget. Morbi consequat accumsan tempus. 
-                    Etiam metus tellus, blandit a dignissim eget, vehicula non quam.
-                </p>
-                <p>
-                    Aliquam erat volutpat. Orci varius natoque penatibus et magnis dis parturient montes, 
-                    nascetur ridiculus mus. In justo sapien, vestibulum in efficitur sit amet, fringilla posuere elit. 
-                    Vestibulum nec commodo nunc, in sagittis dui. Fusce bibendum mauris ac lacus vulputate, 
-                    et congue ligula volutpat. Donec id sem massa. Mauris ex lectus, posuere nec tempus ac, 
-                    iaculis rhoncus urna. Suspendisse sed cursus odio.
-                </p>
-                <p>
-                    Suspendisse condimentum bibendum erat, vel sodales mauris vulputate at. Fusce libero tellus, 
-                    porttitor sit amet tempor et, cursus in nibh. Donec ipsum arcu, convallis eu ex non, suscipit sollicitudin enim. 
-                    Ut a auctor ex. Phasellus volutpat ultrices neque eget cursus. In a erat venenatis turpis interdum facilisis. 
-                    Praesent mollis urna quis commodo accumsan. Phasellus interdum, ligula sit amet mattis rhoncus, 
-                    tellus ex semper mi, at congue orci tellus at nulla. Donec ac augue eu nibh feugiat iaculis. 
-                    Etiam laoreet dictum purus, at ultrices urna euismod vitae. Sed finibus orci a dictum dapibus. 
-                    Vivamus ullamcorper, sem quis porttitor dapibus, magna felis fringilla augue, sed elementum diam est nec justo. 
-                    Sed feugiat pellentesque enim, vitae viverra erat vestibulum eget. Morbi consequat accumsan tempus. 
-                    Etiam metus tellus, blandit a dignissim eget, vehicula non quam.
-                </p>
-                <p>
-                    Aliquam erat volutpat. Orci varius natoque penatibus et magnis dis parturient montes, 
-                    nascetur ridiculus mus. In justo sapien, vestibulum in efficitur sit amet, fringilla posuere elit. 
-                    Vestibulum nec commodo nunc, in sagittis dui. Fusce bibendum mauris ac lacus vulputate, 
-                    et congue ligula volutpat. Donec id sem massa. Mauris ex lectus, posuere nec tempus ac, 
-                    iaculis rhoncus urna. Suspendisse sed cursus odio.
-                </p>
-                <p>
-                    Suspendisse condimentum bibendum erat, vel sodales mauris vulputate at. Fusce libero tellus, 
-                    porttitor sit amet tempor et, cursus in nibh. Donec ipsum arcu, convallis eu ex non, suscipit sollicitudin enim. 
-                    Ut a auctor ex. Phasellus volutpat ultrices neque eget cursus. In a erat venenatis turpis interdum facilisis. 
-                    Praesent mollis urna quis commodo accumsan. Phasellus interdum, ligula sit amet mattis rhoncus, 
-                    tellus ex semper mi, at congue orci tellus at nulla. Donec ac augue eu nibh feugiat iaculis. 
-                    Etiam laoreet dictum purus, at ultrices urna euismod vitae. Sed finibus orci a dictum dapibus. 
-                    Vivamus ullamcorper, sem quis porttitor dapibus, magna felis fringilla augue, sed elementum diam est nec justo. 
-                    Sed feugiat pellentesque enim, vitae viverra erat vestibulum eget. Morbi consequat accumsan tempus. 
-                    Etiam metus tellus, blandit a dignissim eget, vehicula non quam.
-                </p>
+            <div class="article-contents post-contents">
+                <h2>About me</h2>
+                <p><em><b>I'm Maiko Miyazaki, a Web Developer and apprentice Software Engineer living in Dublin, Ireland. I love to learn all things about Software development.</b></em></p>
+                <img src={face} className="face right"/>
+                <p>As a self-taught web developer, I have created many personal projects and websites for clients using WordPress, PHP, JavaScript, and many other technologies.</p>
+                <p>Currently I am taking software development apprenticeship programme organized by <a href="https://fit.ie/">FIT</a> who provide us both off and on-the-job training.
+                    At the moment, I am taking off-the-job training full-time at <a href="https://whitehallcollege.com/">Whitehall College</a> learning together with many other apprentices.</p>
+
+                <h2>About this website</h2>
+                <p>This website is hosted on Netlify. For the front end, I used Gatsby and Sass.</p>
+                <p>The pictures I'm using for the thumbnails are taken in my hometown, Yao prefecture in Osaka.</p>
             </div>
         </article>    
     </div>
