a97faa3037491215ed3981ccbf60479f7355029f
miyazakimaiko <myzkmik19922@gmail.com>
2021-06-20 13:11:09 +0100

    Added projects

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -115,8 +115,52 @@ const Footer = () => (
                     </li>
 
                     <li>
+                        <a href="https://waltz-coffee.com/">
+                            Waltz Coffee Roastery
+                            <svg 
+                                xmlns="http://www.w3.org/2000/svg" 
+                                fill="currentColor" 
+                                className="bi bi-box-arrow-up-right icon" 
+                                viewBox="0 0 16 16"
+                            >
+                                <path 
+                                    fill-rule="evenodd" 
+                                    d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 
+                                    1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"
+                                />
+                                <path 
+                                    fill-rule="evenodd" 
+                                    d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"
+                                />
+                            </svg>
+                        </a>
+                    </li>
+
+                    <li>
                         <a href="https://best-song.net/">
                             Your Best Song
+                            <svg 
+                                xmlns="http://www.w3.org/2000/svg" 
+                                fill="currentColor" 
+                                className="bi bi-box-arrow-up-right icon" 
+                                viewBox="0 0 16 16"
+                            >
+                                <path 
+                                    fill-rule="evenodd" 
+                                    d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 
+                                    1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"
+                                />
+                                <path 
+                                    fill-rule="evenodd" 
+                                    d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"
+                                />
+                            </svg>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="http://osakatourism.com.s3-website-eu-west-1.amazonaws.com/">
+                            Osaka Tourism Website
                             <svg 
                                 xmlns="http://www.w3.org/2000/svg" 
                                 fill="currentColor" 
